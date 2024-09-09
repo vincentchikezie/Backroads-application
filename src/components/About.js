320a0e65143d49e572ff28bfb9bc931d43723312
Vincent Chikezie <vincentchikezie@VincentChikezie.lan>
2024-09-09 22:55:56 +0100

    back roads application

--- src/components/About.js
+++ src/components/About.js
@@ -21,7 +21,7 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#about" className="btn">
+          <a href="#" className="btn">
             read more
           </a>
         </article>
