c9209eb399a1386b01829c13b13b7a11bfc518df
Vincent Chikezie <vincentchikezie@VincentChikezie.lan>
2024-09-09 15:22:15 +0100

    back roads application

---
+++ src/components/Footer.js
@@ -0,0 +1,33 @@
+import { socialLinks } from "../data";
+import PageLinks from "./pageLinks";
+
+const Footer = () => {
+  return (
+    <footer className="section footer">
+      <PageLinks parentClass="footer-links" itemClass="footer-link" />
+
+      <ul className="footer-icons">
+        {socialLinks.map((link) => {
+          const { id, href, icon } = link;
+          return (
+            <li key={id}>
+              <a
+                href={href}
+                target="_blank"
+                className="footer-icon"
+                rel="noreferrer"
+              >
+                <i className={icon}></i>
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <p className="copyright">
+        copyright &copy; Backroads travel tours company
+        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+      </p>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/components/pageLinks.js
@@ -0,0 +1,18 @@
+import { pageLinks as linksData } from "../data";
+
+const PageLinks = ({ parentClass, itemClass }) => {
+  return (
+    <ul className={parentClass} id="nav-links">
+      {linksData.map((link) => {
+        return (
+          <li key={link.id}>
+            <a href={link.href} className={itemClass}>
+              {link.text}
+            </a>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+export default PageLinks;
